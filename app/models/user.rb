75f83ad0e470aa168deab359eeee53929eea4f5c
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-04 21:05:03 +0900

    バリデーションの追加（正規表現）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/+\z/ } 
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/+\z/ } 
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,        presence: true
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
 end
