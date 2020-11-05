1a23eac024c810b719183b4eac05bdfda9cdf9c4
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-05 12:22:19 +0900

    バリデーションの追加（正規表現２）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: PASSWORD_REGEX }
+  validates :password_confirmation, presence: true  
   validates :nickname,        presence: true
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/+\z/ } 
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/+\z/ } 
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } 
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } 
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,        presence: true
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
 end
