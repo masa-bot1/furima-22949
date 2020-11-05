abc34296d055f16709b7df4b443a216c1e4bf7cc
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-05 21:11:41 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: {with: PASSWORD_REGEX }
-  validates :password_confirmation, presence: true  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }
+  validates :password_confirmation, presence: true
   validates :nickname,        presence: true
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } 
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } 
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,        presence: true
-
 end
