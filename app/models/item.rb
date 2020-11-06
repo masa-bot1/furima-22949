4bc9b6c401e337f0055a7da56978769e5e033b01
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-06 18:52:55 +0900

    ActiveHashの導入とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :delivery_day
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :postage_id, numericality: { other_than: 1 } 
+  validates :region_id, numericality: { other_than: 1 } 
+  validates :delivery_days_id, numericality: { other_than: 1 } 
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9,999,999 }
+
 end
