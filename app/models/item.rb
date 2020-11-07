a23e5f10fda3d739b3ac140fe1a7f6a830add5f6
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-07 17:26:41 +0900

    バリデーションの共通化

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 } 
-  validates :postage_id, numericality: { other_than: 1 } 
-  validates :region_id, numericality: { other_than: 1 } 
-  validates :delivery_days_id, numericality: { other_than: 1 } 
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, format: { with: /\A[0-9]+\z/ },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :postage_id
+    validates :region_id
+    validates :delivery_days_id
+  end
 
 end
