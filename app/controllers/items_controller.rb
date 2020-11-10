3bd3f47fefcb7e3318e31d56268cd9ccaa7ac8f2
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-10 17:07:19 +0900

    コメントへの修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    redirect_to action: :index unless current_user.id == @item.user_id
     redirect_to root_path if @item.destroy
   end
 
