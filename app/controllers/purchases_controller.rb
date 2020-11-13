54e4feedab39fddc35ab74d39af86ee3a758616c
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-13 14:07:47 +0900

    決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,7 +6,14 @@ class PurchasesController < ApplicationController
 
   def create
     @user_purchase = UserPurchase.new(purchase_params)
+    @item = Item.find(params[:item_id])
     if @user_purchase.valid?
+      Payjp.api_key = "sk_test_9a30a689fbe09ba235b58fab"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price, # 商品の値段
+        card: purchase_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @user_purchase.save
       redirect_to root_path
     else
@@ -17,6 +24,6 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:user_purchase).permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:user_purchase).permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,6 +1,6 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number, :user_id, :item_id, :purchase_id
+  attr_accessor :postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number, :user_id, :item_id, :purchase_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
