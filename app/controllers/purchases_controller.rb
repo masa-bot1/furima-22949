9bc67a591ad51d3f5a5340e7767e0eb24074444e
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-14 14:42:07 +0900

    ミスの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,11 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: :index
-  before_action :set_item
+  before_action :set_item, only: [:index, :create]
 
   def index
     @user_purchase = UserPurchase.new
-    redirect_to root_path if current_user.id == @item.user_id && @item.purchase.present?
+    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if @item.purchase.present?
   end
 
   def create
